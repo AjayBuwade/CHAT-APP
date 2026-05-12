bde2afe8f51646f15e036b04f0264c2e0463164a
Ajay Buwade <ajaybuwade80@gmail.com>
2026-05-12 16:52:36 +0530

    fixed api

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
     useEffect(() => {
         if (authUser) {
             // FIX: 'https' ko badal kar 'http' kar diya
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chat-app-heao.onrender.com", {
                 query: {
                     userId: authUser._id,
                 },
