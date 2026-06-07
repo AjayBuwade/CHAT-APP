e9ed3db08350f48ba0278136c3a34439251026b2
Ajay Buwade <ajaybuwade80@gmail.com>
2026-06-07 12:27:43 +0530

    fixed cross domain cookies

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,12 +5,12 @@ const generateTokenAndSetCookie = (userId, res) => {
 		expiresIn: "15d",
 	});
 
-	res.cookie("jwt", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-		secure: process.env.NODE_ENV !== "development",
-	});
+res.cookie("jwt", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+    httpOnly: true, // prevents XSS attacks
+    sameSite: "none", 
+    secure: true,     
+});
 };
 
 export default generateTokenAndSetCookie;
